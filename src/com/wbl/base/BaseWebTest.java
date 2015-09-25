ee5141a24660c8160f90a2e135ddb8a34abc62ad
Pavi <pavitard@whitebox-learning.com>
2015-09-24 17:16:08 -0700

    refactoring and adding login tests

--- src/com/wbl/base/BaseWebTest.java
+++ src/com/wbl/base/BaseWebTest.java
@@ -1,5 +1,6 @@
 package com.wbl.base;
 
+import com.wbl.utils.ExcelUtils;
 import com.wbl.utils.web.PageDriver;
 import com.wbl.utils.web.WBy;
 import org.testng.annotations.AfterSuite;
@@ -11,11 +12,13 @@ import org.testng.annotations.BeforeSuite;
 public abstract class BaseWebTest extends BaseTest {
 
     public PageDriver driver;
+    public ExcelUtils excelUtils;
 
     @BeforeSuite
     public void beforeSuite() {
         driver = new PageDriver(_config);
         WBy.loadJsonMap(String.format("%s/locators.json", System.getProperty("user.dir")));
+        excelUtils = new ExcelUtils();
     }
 
     @AfterSuite
