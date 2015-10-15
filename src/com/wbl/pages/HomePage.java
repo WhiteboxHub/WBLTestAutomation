a6fea1f36f4921159bf981c9e6ecdb480254e28a
Innovapath <training@innova-path.com>
2015-10-15 14:04:38 -0700

    adding check for js executor

--- src/com/wbl/pages/HomePage.java
+++ src/com/wbl/pages/HomePage.java
@@ -1,13 +1,10 @@
 package com.wbl.pages;
 
+import com.wbl.utils.web.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import com.wbl.pages.PortalPage;
-import com.wbl.utils.web.HtmlElement;
-import com.wbl.utils.web.PageDriver;
-import com.wbl.utils.web.WBy;
-import com.wbl.utils.web.WJavascriptExecutor;
 
 /**
  * Created by svelupula on 8/14/2015.
@@ -32,8 +29,14 @@ public class HomePage extends PortalPage {
 	        driver.visibilityWait(WBy.get("header.social.fblink"));
 	        WebDriver wd = driver.getWebDriver();
 	        WebElement element = wd.findElement(WBy.get("header.social.fblink"));
-	        WJavascriptExecutor js = new WJavascriptExecutor(wd,element);
-	        js.executeScript("arguments[0].click();");
+            if(driver.getBrowser() != Browsers.HtmlUnit) {
+                WJavascriptExecutor js = new WJavascriptExecutor(wd, element);
+                js.executeScript("arguments[0].click();");
+            }
+            else
+            {
+                element.click();
+            }
         }
         catch(Exception e)
         {
